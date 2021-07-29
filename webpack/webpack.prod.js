3e83a6319059faf35abf67608c5458c3e46c0157
Daniel Tomlinson <daniel.tomlinson@skybettingandgaming.com>
2021-07-29 12:14:31 +0100

    adding files

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,59 @@
+const AssetsPlugin = require("assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+//   devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "../public"),
+    filename: "[name].min.js",
+    chunkFilename: "[id].[name].min.js",
+    publicPath: "/",
+  },
+  entry: {
+    main: path.resolve(__dirname, "../src/main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new AssetsPlugin({
+      filename: "assets.json",
+      path: path.resolve(__dirname, "../public"),
+      prettyPrint: true,
+      fullPath: false,
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["../public/assets.json"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].min.css",
+      chunkFilename: "[name].[contenthash].min.css",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
